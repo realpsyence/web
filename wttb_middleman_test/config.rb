707b21bf5b9d8766fc79f0c16e3622acc2227f07
David Cummings <david.logan.cummings@gmail.com>
2012-12-08 02:38:52 -0500

    Modified site using Middleman-blog and bootstrap css currently a copy of what's up on WTTB but with new blogging stuff and done layout style. Should work to be the new working version

--- wttb_middleman_test/config.rb
+++ wttb_middleman_test/config.rb
@@ -1,9 +1,39 @@
 ###
+# Blog settings
+###
+
+ Time.zone = "America/New_York"
+
+activate :blog do |blog|
+  blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+  page "blog/*", :layout => :article_layout 
+end
+
+page "/feed.xml", :layout => false
+
+### 
 # Compass
 ###
 
 # Susy grids in Compass
-# First: gem install susy --pre
+# First: gem install susy
 # require 'susy'
 
 # Change Compass configuration
@@ -16,13 +46,13 @@
 ###
 
 # Per-page layout changes:
-#
+# 
 # With no layout
 # page "/path/to/file.html", :layout => false
-#
+# 
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-#
+# 
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -57,21 +87,21 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-
+  
   # Minify Javascript on build
   # activate :minify_javascript
-
+  
   # Enable cache buster
   # activate :cache_buster
-
+  
   # Use relative URLs
   # activate :relative_assets
-
+  
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-
+  
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
