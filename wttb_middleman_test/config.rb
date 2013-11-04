b44976d5408b469ada0df1ee3476e69a81dd1c7b
David Cummings <david.logan.cummings@gmail.com>
2013-11-03 21:10:58 -0800

    Modified some layout to create a centered look. Mostly an airgap span2 on the left, a centered navbar and stuff.
    Also some changes to the blog summaries and index page. Cool thing with
    READMORE

--- wttb_middleman_test/config.rb
+++ wttb_middleman_test/config.rb
@@ -11,8 +11,8 @@ activate :blog do |blog|
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
   # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 500 
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
